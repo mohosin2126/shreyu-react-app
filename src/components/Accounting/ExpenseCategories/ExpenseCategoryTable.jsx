42b6bf3a53126ef04be4fb3eb8f48b2846bc760c
Mohosin212601~ <mohosin2126@gmail.com>
2024-05-04 14:18:23 +0600

    modify expenses category with modal

--- src/components/Accounting/ExpenseCategories/ExpenseCategoryTable.jsx
+++ src/components/Accounting/ExpenseCategories/ExpenseCategoryTable.jsx
@@ -1,8 +1,8 @@
-import { Button, ButtonGroup, Card, Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, ButtonGroup, Card, Col, Form, InputGroup, Modal, Row } from "react-bootstrap";
 import PageTitle from "../../PageTitle";
-import { records as data } from "./data";
 import Table from "../../Table";
-import { Link } from "react-router-dom";
+import { records as data } from "./data";
 
 const columns = [
     {
@@ -20,41 +20,38 @@ const columns = [
         accessor: "icon",
         Cell: ({ row }) => (
             <div>
-                <i class="bi bi-pencil-square fs-3 p-2 text-primary"></i>
-                <i class="bi bi-trash fs-3 p-2 text-primary"></i>
+                <i className="bi bi-pencil-square fs-3 p-2 text-primary"></i>
+                <i className="bi bi-trash fs-3 p-2 text-primary"></i>
             </div>
         ),
     },
 ];
 
 const sizePerPageList = [
-    {
-        text: "5",
-        value: 5,
-    },
-    {
-        text: "10",
-        value: 10,
-    },
-    {
-        text: "25",
-        value: 25,
-    },
-    {
-        text: "All",
-        value: data.length,
-    },
+    { text: "5", value: 5 },
+    { text: "10", value: 10 },
+    { text: "25", value: 25 },
+    { text: "All", value: data.length },
 ];
 
 const ExpenseCategoryTable = () => {
+    const [showScrollableModal, setShowScrollableModal] = useState(false);
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+    };
+
     return (
         <>
-
             <PageTitle
                 breadCrumbItems={[
-
                     { label: "Expense Categories", path: "/components/accounting-expense-category", active: true },
-
                 ]}
                 title={"Expenses"}
             />
@@ -62,28 +59,47 @@ const ExpenseCategoryTable = () => {
                 <Col>
                     <Card>
                         <Card.Body>
-
-                            <Row class="border">
+                            <Row className="border">
                                 <Col xs={12} md={4}>
                                     <h4 className="header-title">Expense Categories Table</h4>
-                                </Col >
-                                <Col xs={12} md={6} class="align-center">
+                                </Col>
+                                <Col xs={12} md={6} className="align-center">
                                     <ButtonGroup className="mb-2">
-                                    <div class="btn-group" role="group" aria-label="Basic outlined example">
-                                            <button type="button" class="btn btn-outline-primary"><i class="bi bi-files fs-3 p-2"></i>copy</button>
-                                            <button type="button" class="btn btn-outline-primary"><i class="bi bi-file-earmark-excel  fs-3 p-2"></i>Excel</button>
-                                            <button type="button" class="btn btn-outline-primary"><i class="bi bi-file-ruled fs-3 p-2"></i>CSB</button>
-                                            <button type="button" class="btn btn-outline-primary"><i class="bi bi-file-earmark-pdf fs-3 p-2"></i>PDF</button>
+                                        <div className="btn-group" role="group" aria-label="Basic outlined example">
+                                            <button type="button" className="btn btn-outline-primary"><i className="bi bi-files fs-3 p-2"></i>copy</button>
+                                            <button type="button" className="btn btn-outline-primary"><i className="bi bi-file-earmark-excel  fs-3 p-2"></i>Excel</button>
+                                            <button type="button" className="btn btn-outline-primary"><i className="bi bi-file-ruled fs-3 p-2"></i>CSB</button>
+                                            <button type="button" className="btn btn-outline-primary"><i className="bi bi-file-earmark-pdf fs-3 p-2"></i>PDF</button>
                                         </div>
                                     </ButtonGroup>
                                 </Col>
                                 <Col xs={12} md={2}>
-                                    <Link to="/components/accounting-create-expense-category">
-                                        <Button class="primary ">
+                                    <Button onClick={() => setShowScrollableModal(true)} className="primary ">
+                                        <i className="bi bi-plus-lg p-2"></i>
+                                        <span>Create</span>
+                                    </Button>
+                                    <Modal show={showScrollableModal} onHide={() => setShowScrollableModal(false)} scrollable>
+    <Modal.Header closeButton>
+        <Modal.Title as="h5">Add Expense Category</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        <Form className='addExpenseCategoryForm' noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="validationCategory">
+                <Form.Label>Category</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Expense Category Name"
+                    required
+                />
+                <Form.Control.Feedback type="invalid">
+                    Please provide an expense category name.
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+        </Form>
+    </Modal.Body>
+</Modal>
 
-                                            <i class="bi bi-plus-lg p-2"></i>
-                                            <span>Create</span>
-                                        </Button></Link>
                                 </Col>
                             </Row>
                             <Table
--- src/components/Accounting/Expenses/ExpensesTable.jsx
+++ src/components/Accounting/Expenses/ExpensesTable.jsx
@@ -1,8 +1,14 @@
-import { Button, ButtonGroup, Card, Col, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Button, ButtonGroup, Card, Col, Form, Modal, Row } from "react-bootstrap";
 import Table from "../../Table";
 import { records as data } from "./data";
 import PageTitle from "../../PageTitle";
+import SimpleMDEReact from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import Category from "./Category";
+import DatePick from "./Date";
+import Doctor from "./Doctor";
+
 const columns = [
     {
         Header: "#",
@@ -29,33 +35,33 @@ const columns = [
         accessor: "icon",
         Cell: ({ row }) => (
             <div>
-                <i class="bi bi-pencil-square fs-3 p-2 text-primary"></i>
-                <i class="bi bi-trash fs-3 p-2 text-primary"></i>
+                <i className="bi bi-pencil-square fs-3 p-2 text-primary"></i>
+                <i className="bi bi-trash fs-3 p-2 text-primary"></i>
             </div>
-
-
         ),
     },
 ];
+
 const sizePerPageList = [
-    {
-        text: "5",
-        value: 5,
-    },
-    {
-        text: "10",
-        value: 10,
-    },
-    {
-        text: "25",
-        value: 25,
-    },
-    {
-        text: "All",
-        value: data.length,
-    },
+    { text: "5", value: 5 },
+    { text: "10", value: 10 },
+    { text: "25", value: 25 },
+    { text: "All", value: data.length },
 ];
+
 const ExpensesTable = () => {
+    const [showScrollableModal, setShowScrollableModal] = useState(false);
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+    };
+
     return (
         <>
             <PageTitle
@@ -71,23 +77,66 @@ const ExpensesTable = () => {
                             <Row>
                                 <Col xs={12} md={4}>
                                     <h4 className="header-title">Expenses</h4>
-                                </Col >
-                                <Col xs={12} md={6} class="align-center ">
-                                    <ButtonGroup className="mb-2 ">
-                                    <div class="btn-group" role="group" aria-label="Basic outlined example">
-                                            <button type="button" class="btn btn-outline-primary"><i class="bi bi-files fs-3 p-2"></i>copy</button>
-                                            <button type="button" class="btn btn-outline-primary"><i class="bi bi-file-earmark-excel  fs-3 p-2"></i>Excel</button>
-                                            <button type="button" class="btn btn-outline-primary"><i class="bi bi-file-ruled fs-3 p-2"></i>CSB</button>
-                                            <button type="button" class="btn btn-outline-primary"><i class="bi bi-file-earmark-pdf fs-3 p-2"></i>PDF</button>
+                                </Col>
+                                <Col xs={12} md={6} className="align-center">
+                                    <ButtonGroup className="mb-2">
+                                        <div className="btn-group" role="group" aria-label="Basic outlined example">
+                                            <button type="button" className="btn btn-outline-primary"><i className="bi bi-files fs-3 p-2"></i>copy</button>
+                                            <button type="button" className="btn btn-outline-primary"><i className="bi bi-file-earmark-excel  fs-3 p-2"></i>Excel</button>
+                                            <button type="button" className="btn btn-outline-primary"><i className="bi bi-file-ruled fs-3 p-2"></i>CSB</button>
+                                            <button type="button" className="btn btn-outline-primary"><i className="bi bi-file-earmark-pdf fs-3 p-2"></i>PDF</button>
                                         </div>
                                     </ButtonGroup>
                                 </Col>
                                 <Col xs={12} md={2}>
-                                    <Link to="/components/accounting-create-expenses">
-                                        <Button class="primary ">
-                                            <i class="bi bi-plus-lg p-2"></i>
-                                            <span>Create</span>
-                                        </Button></Link>
+                                    <Button onClick={() => setShowScrollableModal(true)} className="primary">
+                                        <i className="bi bi-plus-lg p-2"></i>
+                                        <span>Create</span>
+                                    </Button>
+                                    <Modal show={showScrollableModal} onHide={() => setShowScrollableModal(false)} scrollable>
+                                        <Modal.Header closeButton>
+                                            <Modal.Title>Add Expense Category</Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            <div>
+                                                <h4>Create Expenses</h4>
+                                                <div className="container">
+                                                    <div className="row">
+                                                        <div className="row">
+                                                            <Card.Body>
+                                                                <p className="mb-1 fw-bold">Category</p>
+                                                                <Category />
+                                                            </Card.Body>
+                                                        </div>
+                                                        <div className="row">
+                                                            <Card.Body>
+                                                                <DatePick />
+                                                            </Card.Body>
+                                                        </div>
+                                                        <div className="col">
+                                                            <Card.Body>
+                                                                <p className="mb-1 fw-bold">Doctor</p>
+                                                                <Doctor />
+                                                            </Card.Body>
+                                                        </div>
+                                                        <div className="col">
+                                                            <Card.Body>
+                                                                <p className="mb-1 fw-bold">Amount</p>
+                                                                <Form.Group as={Row}>
+                                                                    <Col lg={10}>
+                                                                        <Form.Control id="example-number" type="number" name="number" />
+                                                                    </Col>
+                                                                </Form.Group>
+                                                            </Card.Body>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <h4 className="header-title mt-0 mb-1">Notes</h4>
+                                                <SimpleMDEReact id="1" options={{ autofocus: true }} />
+                                                <Button className="primary width-xs"><i className="bi bi-check-lg p-2"></i><span>Save</span></Button>
+                                            </div>
+                                        </Modal.Body>
+                                    </Modal>
                                 </Col>
                             </Row>
                             <Table
